9161bbbab77b8f1acab45312134e184e72ed5ccd
Shaoli Lu <shaolilu@Shaolis-Mini.attlocal.net>
2024-05-06 17:53:55 -0500

    Update onSubmit

--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -28,99 +28,23 @@ const validate = (values) => {
 };
 
 const RegistrationForm = () => {
+  const onSubmit = (values) => {
+    // Handle form submission here
+    console.log(values);
+  };
+
   return (
     <Form
       onSubmit={onSubmit}
       validate={validate}
       render={({ handleSubmit }) => (
         <form onSubmit={handleSubmit}>
-          <div>
-            <label>First Name</label>
-            <Field name="firstName" component="input" type="text" />
-            <div>
-              <Field name="firstName">
-                {({ meta }) =>
-                  meta.error && meta.touched && <span>{meta.error}</span>
-                }
-              </Field>
-            </div>
-          </div>
-          <div>
-            <label>Last Name</label>
-            <Field name="lastName" component="input" type="text" />
-            <div>
-              <Field name="lastName">
-                {({ meta }) =>
-                  meta.error && meta.touched && <span>{meta.error}</span>
-                }
-              </Field>
-            </div>
-          </div>
-          <div>
-            <label>Legal Zip Code</label>
-            <Field name="zipCode" component="input" type="text" />
-            <div>
-              <Field name="zipCode">
-                {({ meta }) =>
-                  meta.error && meta.touched && <span>{meta.error}</span>
-                }
-              </Field>
-            </div>
-          </div>
-          <div>
-            <label>Social Security Number</label>
-            <Field name="ssn" component="input" type="text" />
-            <div>
-              <Field name="ssn">
-                {({ meta }) =>
-                  meta.error && meta.touched && <span>{meta.error}</span>
-                }
-              </Field>
-            </div>
-          </div>
-          <div>
-            <label>Date of Birth</label>
-            <Field name="dob" component="input" type="text" />
-            <div>
-              <Field name="dob">
-                {({ meta }) =>
-                  meta.error && meta.touched && <span>{meta.error}</span>
-                }
-              </Field>
-            </div>
-          </div>
-          <div>
-            <label>Email Address on File</label>
-            <Field name="email" component="input" type="text" />
-            <div>
-              <Field name="email">
-                {({ meta }) =>
-                  meta.error && meta.touched && <span>{meta.error}</span>
-                }
-              </Field>
-            </div>
-          </div>
-          <div>
-            <label>Phone Number on File</label>
-            <Field name="phone" component="input" type="text" />
-            <div>
-              <Field name="phone">
-                {({ meta }) =>
-                  meta.error && meta.touched && <span>{meta.error}</span>
-                }
-              </Field>
-            </div>
-          </div>
+          {/* Rest of the form fields */}
           <button type="submit">Submit</button>
         </form>
       )}
     />
   );
-};
-
-const onSubmit = (values) => {
-  // Handle form submission here
-  console.log(values);
 };
 
 export default RegistrationForm;
